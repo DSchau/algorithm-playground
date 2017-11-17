72d7a7c172baa0226df40aef01c0d7fa14fd9496
Dustin Schau <DSchau@users.noreply.github.com>
2017-11-16 23:43:48 -0500

    build: optimize for production (#3)

---
+++ src/components/Timer/index.js
@@ -0,0 +1,105 @@
+import React, { Component }  from 'react';
+import styled, { css } from 'react-emotion';
+import { darken } from 'polished';
+
+import { SANS_SERIF, SLIDE_UP, Z_INDEX_SUPER } from '../../style';
+
+const Container = styled.div`
+  position: absolute;
+  bottom: 7.5%;
+  right: 2.5%;
+  width: auto;
+  padding: 0.5rem 1rem;
+  margin: 0 auto;
+  box-sizing: border-box;
+  border: 1px solid transparent;
+  animation: ${SLIDE_UP} 325ms cubic-bezier(0.390, 0.575, 0.565, 1.000);
+  z-index: ${Z_INDEX_SUPER};
+  font-family: ${SANS_SERIF};
+
+  ${({ theme }) => css`
+    background-color: ${theme[theme.primary].base};
+    border-color: ${darken(0.05, theme[theme.primary].base)};
+  `};
+`;
+
+const TimerContainer = styled.div`
+  text-align: center;
+`;
+
+const Title = styled.h1`
+  margin: 0;
+  padding: 0;
+  font-size: 12px;
+  display: inline-block;
+  text-transform: uppercase;
+
+  color: ${({ theme }) => theme[theme.primary].text};
+`;
+
+const Button = styled.button`
+  outline: none;
+  border-width: 0;
+  border-radius: 0.125rem;
+  margin-left: 1rem;
+  padding: 0.25rem 0.5rem;
+  box-sizing: border-box;
+
+  font-family: ${SANS_SERIF};
+
+  ${({ theme }) => css`
+    background-color: ${theme[theme.primary].text};
+    color: ${theme[theme.primary].base};
+  `};
+`;
+
+export class Timer extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      seconds: props.duration / 1000
+    };
+  }
+
+  componentDidMount() {
+    this.interval = setInterval(() => {
+      const seconds = this.state.seconds - 1;
+      let stateUpdatedCallback = () => {};
+      if (seconds === 0) {
+        clearInterval(this.interval);
+        stateUpdatedCallback = this.handleElapsed;
+      }
+      this.setState(
+        {
+          seconds
+        },
+        stateUpdatedCallback
+      );
+    }, 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+
+  handleElapsed = () => {
+    this.props.onElapsed();
+  };
+
+  render() {
+    if (this.state.seconds === 0) {
+      return null;
+    }
+    return (
+      <Container>
+        <TimerContainer>
+          <Title>Content Updated</Title>
+          <Button onClick={this.handleElapsed}>
+            Refresh? ({this.state.seconds})
+          </Button>
+        </TimerContainer>
+      </Container>
+    );
+  }
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,58 +1,79 @@
 const path = require('path');
+const webpack = require('webpack');
+const assign = require('webpack-config-assign');
+
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-module.exports = ({ env }) => ({
-  devtool: 'source-map',
-  entry: {
-    bundle: ['./src/index']
-  },
-  output: {
-    filename: '[name].js',
-    path: path.join(__dirname, 'dist')
-  },
-  module: {
-    rules: [
-      {
-        test: /\.jsx?/,
-        include: [
-          path.join(__dirname, 'src'),
-          path.join(__dirname, 'src/algorithms/index')
-        ],
-        use: ['babel-loader'],
-        exclude: [/\.worker.js$/]
-      },
-      {
-        test: /\.worker\.js$/,
-        include: path.join(__dirname, 'src'),
-        use: ['worker-loader', 'babel-loader']
-      },
-      {
-        test: /\.js$/,
-        use: ['raw-loader'],
-        include: path.join(__dirname, 'src/algorithms'),
-        exclude: path.join(__dirname, 'src/algorithms/index')
-      },
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader']
-      },
-      {
-        test: /\.woff2?/,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              name: 'static/media/[name].[hash:8].[ext]'
+module.exports = function webpackConfig({ environment } = {}) {
+  const base = {
+    devtool: 'source-map',
+    entry: {
+      bundle: ['./src/index'],
+      vendor: ['react', 'react-dom', 'codemirror']
+    },
+    output: {
+      filename: '[name].js',
+      path: path.join(__dirname, 'dist')
+    },
+    module: {
+      rules: [
+        {
+          test: /\.jsx?/,
+          include: [
+            path.join(__dirname, 'src'),
+            path.join(__dirname, 'src/algorithms/index')
+          ],
+          use: ['babel-loader'],
+          exclude: [
+            /\.worker.js$/,
+            'proxy-polyfill'
+          ]
+        },
+        {
+          test: /\.worker\.js$/,
+          include: path.join(__dirname, 'src'),
+          use: ['worker-loader', 'babel-loader']
+        },
+        {
+          test: /\.js$/,
+          use: ['raw-loader'],
+          include: path.join(__dirname, 'src/algorithms'),
+          exclude: path.join(__dirname, 'src/algorithms/index')
+        },
+        {
+          test: /\.css$/,
+          use: ['style-loader', 'css-loader']
+        },
+        {
+          test: /\.woff2?/,
+          use: [
+            {
+              loader: 'file-loader',
+              options: {
+                name: 'static/media/[name].[hash:8].[ext]'
+              }
             }
-          }
-        ]
-      }
-    ]
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      inject: 'body',
-      template: path.join(__dirname, 'public/index.html')
-    })
-  ]
-});
+          ]
+        }
+      ]
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        inject: 'body',
+        template: 'public/index.html'
+      }),
+      new webpack.DefinePlugin({
+        'process.env.NODE_ENV': JSON.stringify(environment)
+      })
+    ],
+    externals: {
+      React: 'react',
+      ReactDOM: 'react-dom'
+    },
+    stats: {
+      children: false
+    }
+  };
+
+  return assign(base, require(`./webpack.config.${environment}`));
+};
