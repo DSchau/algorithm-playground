5bfb5a73f3a1457d448150bf681cf82b93a0aad3
Dustin Schau <dustinschau@gmail.com>
2017-11-16 21:55:10 -0600

    tweak shell sort

--- src/algorithms/sorting/shell-sort.js
+++ src/algorithms/sorting/shell-sort.js
@@ -1,6 +1,7 @@
 export function shellSort(arr) {
-  for (var h = arr.length; h > 0; h = parseInt(h / 2)) {
-    for (var i = h; i < arr.length; i++) {
+  const len = arr.length;
+  for (var h = len; h > 0; h = parseInt(h / 2)) {
+    for (var i = h; i < len; i++) {
       var k = arr[i];
       for (var j = i; j >= h && k < arr[j - h]; j -= h) {
         arr[j] = arr[j - h];
