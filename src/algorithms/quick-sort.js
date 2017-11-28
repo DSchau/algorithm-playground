80194b30d4a044c9346467ecca51698aa49c383b
Dustin Schau <DSchau@users.noreply.github.com>
2017-11-27 23:44:14 -0500

    style: wrap algorithm descriptions at 80 characters (#28)

--- src/algorithms/quick-sort.js
+++ src/algorithms/quick-sort.js
@@ -1,6 +1,10 @@
 /*
  * CODE: http://learnjswith.me/quick-sort-in-javascript/
- * Quicksort is a divide and conquer algorithm. Quicksort first divides a large array into two smaller sub-arrays: the low elements and the high elements. Quicksort can then recursively sort the sub-arrays.
+ *
+ * Quicksort is a divide and conquer algorithm. Quicksort first divides a 
+ large array into two smaller sub-arrays: the low elements and the high 
+ elements. Quicksort can then recursively sort the sub-arrays.
+ *
  * MORE INFO: https://en.wikipedia.org/wiki/Quicksort
  */
 function partition(arr, left, right) {
--- src/algorithms/selection-sort.js
+++ src/algorithms/selection-sort.js
@@ -1,6 +1,14 @@
 /*
  * CODE: https://www.nczonline.net/blog/2009/09/08/computer-science-in-javascript-selection-sort/
- * The Selection sort algorithm divides the input list into two parts: the sublist of items already sorted and the sublist of items remaining to be sorted that occupy the rest of the list. Initially, the sorted sublist is empty and the unsorted sublist is the entire input list. The algorithm proceeds by finding the smallest element in the unsorted sublist, exchanging it with the leftmost unsorted element, and moving the sublist boundaries one element to the right
+ *
+ * The Selection sort algorithm divides the input list into two parts: the 
+ sublist of items already sorted and the sublist of items remaining to be 
+ sorted that occupy the rest of the list. Initially, the sorted sublist is 
+ empty and the unsorted sublist is the entire input list. The algorithm 
+ proceeds by finding the smallest element in the unsorted sublist, exchanging 
+ it with the leftmost unsorted element, and moving the sublist boundaries one 
+ element to the right
+ *
  * MORE INFO: https://en.wikipedia.org/wiki/Selection_sort
  */
 export function selectionSort(arr) {
